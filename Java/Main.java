70e4b44b6c5fd0384aa0104469cf5454148777e6
Athay <abdieldeathayde67@gmail.com>
2024-03-03 11:50:03 -0300

    feat: updating class main

--- Java/Main.java
+++ Java/Main.java
@@ -1,31 +1,25 @@
 import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
         tabuada t = new tabuada();
-        t.run();
+        Thread thread1 = new Thread(new tabuada());
+        thread1.start();
+        try {
+            thread1.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
-
 class tabuada implements Runnable {
-
     public int numero;
     public int i;
-
-    public tabuada() {
-        this.numero = numero;
-        this.i = i;
-    }
-
     @Override
     public void run() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Digite um numero: ");
         numero = sc.nextInt();
         i = 1;
-
-        Thread thread1 = new Thread(new tabuada());
-
         while (i < 11) {
             int resultado = numero * i;
             System.out.println("Resultado: " + resultado);
